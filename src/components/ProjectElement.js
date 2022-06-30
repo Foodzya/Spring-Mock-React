32eee0b4eb28047937898c8c898e9c9ca861d9dc
Foodzya <afodzya@gmail.com>
2022-06-30 10:14:15 +0300

    Styling improvements in styles.css. Changes in ProjectElement and ProjectTable components

--- src/components/ProjectElement.js
+++ src/components/ProjectElement.js
@@ -1,19 +1,23 @@
 import React from 'react';
 
-const ProjectElement = (props) => {
-    return (
-        <a href className="whitebox-page-element">
-            <div className="page-element">
-                <div>
-                    <img src={props.image} alt="Spring Boot" className="page-image" />
-                </div>
-                <div>
-                    <h2 className="page-element-header">{props.title} </h2>
-                    <p className="page-element-text">{props.description}</p>
-                </div>
-            </div>
-        </a>
-    )
+const ProjectElement = ({ elements }) => {
+    return <div className='page-container'>
+        {elements.map((element) => {
+            return (
+                <a href key={element.title.toString()} className="whitebox-page-element">
+                    <div className="page-element">
+                        <div>
+                            <img src={element.image} alt="Spring Boot" className="page-image" />
+                        </div>
+                        <div>
+                            <h2 className="page-element-header">{element.title} </h2>
+                            <p className="page-element-text">{element.description}</p>
+                        </div>
+                    </div>
+                </a>
+            );
+        })};
+    </div>
 }
 
 export default ProjectElement;
